7f440ebb16c48a2ec24dd85e37a1ce07919ee926
MPCGomes <mpcgomes2@gmail.com>
2024-06-13 22:55:06 -0300

    PokemonCard Component

---
+++ src/components/PokemonCard/index.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import TypeBadge from '../TypeBadge';
+
+interface PokemonCardProps {
+  id: number;
+  name: string;
+  image: string;
+  types: string[];
+};
+
+const capitalize = (s: string) => s.charAt(0).toUpperCase + s.slice(1);
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ id, name, image, types }) => (
+  <div className={styles.card}>
+    <img
+      className={styles.image}
+      src={image}
+      alt={name}
+    />
+    <div className={styles.details}>
+      <p className={styles.id}>
+        Nº{id.toString().padStart(4, '0')}
+      </p>
+      <h3 className={styles.name}>
+        {capitalize(name)}
+      </h3>
+      <div className={styles.type}>
+        {types.map((type) => (
+          <TypeBadge key={type} type={capitalize(type)} />
+        ))}
+      </div>
+    </div>
+  </div>
+);
+
+export default PokemonCard;
